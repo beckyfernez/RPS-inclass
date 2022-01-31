c03b84d78527cb403f946dd34fd400e3cdf3278c
Rebecca Fernandez <97413846+beckyfernez@users.noreply.github.com>
2022-01-31 17:41:08 -0500

    Worked on Exercise Objectives
    I completed the computer choice and started and have started on the logic behind determining the winner and validating the user input.

--- game.py
+++ game.py
@@ -1,29 +1,54 @@
 
-
-
-#
-# todo: write some Python code here to satisfy the exercise objectives
+#   todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
+
+
+
+# this is the "game.py" file
+print ("Rock, Paper, Scissors, Shoot!")
 
 
 #Ask for a user input
+userChoice = input("Please choose either : 'Rock', 'Paper', or 'Scissors': ")
+print ("You chose : ", userChoice)
+    #eventually I want to convert all variations of "ROCK" into the standard "Rock"
 
-userChoice = input("Choose one of : 'Rock', 'Paper', 'Scissors': ")
-
-print("You chose : ", userChoice)
 
 #Validate user input
+if userChoice == "Rock":
+    print ("You chose : ", userChoice)
+else:
+    print("Sorry that is not a valid input. You have exited the game.")
+    quit()
 
 
+#COMPUTER CHOICE (Generating a computer selection)
+#Use a library of python code - "module"
+#https://docs.python.org/3/library/random.html#random.choice
+# https://www.geeksforgeeks.org/python-select-random-value-from-a-list/
+# https://github.com/prof-rossetti/intro-to-python/blob/main/notes/python/modules/random.md
 
-#Computer coice
-
+import random
+gameOptions = ['Rock', 'Paper', 'Scissors']
+comChoice = random.choice(gameOptions)
+print("The computer chose : ", comChoice)
 
 
 #Determine winner
-
+#when to use elif and its syntax
+if userChoice == comChoice:
+    print ("We have a tie.")
+if userChoice == "Rock" and comChoice == "Paper":
+    print ("Oh, the computer won. It's okay.")
+if userChoice == "Rock" and comChoice == "Scissors":
+    print ("You won!")
 
 
 #Return Final Results
 
+
+#Ask if user would like to play again
+replay = input("Would you like to play again? Choose 'Yes' or 'No' : ")
+if replay == "No":
+    quit()
+if replay == "Yes":

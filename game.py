adeaf75cfdb32db352b065cc1b0ca980503646ec
Rebecca Fernandez <97413846+beckyfernez@users.noreply.github.com>
2022-01-30 21:22:19 -0500

    Asked for user input
    Assigned a variable to an input
    and printed the variable chosen.

--- game.py
+++ game.py
@@ -5,3 +5,25 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+#Ask for a user input
+
+userChoice = input("Choose one of : 'Rock', 'Paper', 'Scissors': ")
+
+print("You chose : ", userChoice)
+
+#Validate user input
+
+
+
+#Computer coice
+
+
+
+#Determine winner
+
+
+
+#Return Final Results
+
